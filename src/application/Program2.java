38537918df60b2a54406f252ad3a5691c31de2dc
Ygor Nacif <ygor.nacif00@gmail.com>
2024-09-10 11:55:24 -0300

    Finalizando o DepartmentDaoJDBC

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -17,18 +20,27 @@ public class Program2 {
 	
 	
 	System.out.println("=== TEST 2: Department Insert ===");
-	Department newDepartment = new Department(7, "VideoGames");
+	Department newDepartment = new Department(9, "VideoGames");
 	departmentDao.insert(newDepartment);
 	System.out.println("Department inserido " + newDepartment.getId());
 	
 	System.out.println("=== TEST 3: Department Update ===");
-	Department newUpdate = new Department(5, "MicroOndas");
+	Department newUpdate = new Department(4, "MicroOndas");
 	departmentDao.update(newUpdate);
 	System.out.println("Department atualizado " + newUpdate.getId());
 	
 	System.out.println("=== TEST 4: Department Delete ===");
-	departmentDao.deleteById(5);
+	departmentDao.deleteById(7);
 	System.out.println("Department deletado");
+	
+	System.out.println("=== TEST 4: Department Delete ===");
+	List<Department> list = new ArrayList<Department>();
+	list = departmentDao.findAll();
+	for(Department obj : list) {
+		System.out.println(obj);
+		
+	}
+	
 	
 	}
 }
